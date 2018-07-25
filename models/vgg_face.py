da8100e9eec29b0712bb4c1f18e4b2f6056168ce
prlz77 <pau.rodri1@gmail.com>
2018-07-25 12:31:44 +0200

    First commit

---
+++ models/vgg_face.py
@@ -0,0 +1,107 @@
+from torch.utils.serialization import load_lua
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2
+
+
+class VGG_16(nn.Module):
+    """
+    Main Class
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.block_size = [2,2,3,3,3]
+        self.conv_1_1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.conv_1_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.conv_2_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.conv_2_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.conv_3_1 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.conv_3_2 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_3_3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_4_1 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
+        self.conv_4_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_4_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_1 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.fc6 = nn.Linear(512*7*7, 4096)
+        self.fc7 = nn.Linear(4096, 4096)
+        self.fc8 = nn.Linear(4096, 2622)
+
+    def load_weights(self, path="pretrained/VGG_FACE.t7"):
+        """ Function to load luatorch weights
+
+        Args:
+            path: path for the luatorch weights
+        """
+        model = load_lua(path, unknown_classes=True)
+        counter = 1
+        block = 1
+        for i, layer in enumerate(model.modules):
+            if hasattr(layer, "weight"):
+                if block <= 5:
+                    self_layer = getattr(self, "conv_%d_%d" %(block, counter))
+                    counter += 1
+                    if counter > self.block_size[block - 1]:
+                        counter = 1
+                        block += 1
+                    self_layer.weight.data[...] = layer.weight.view_as(self_layer.weight)[...]
+                    self_layer.bias.data[...] = layer.bias.view_as(self_layer.bias)[...]
+                else:
+                    self_layer = getattr(self, "fc%d" %(block))
+                    block += 1
+                    self_layer.weight.data[...] = layer.weight.view_as(self_layer.weight)[...]
+                    self_layer.bias.data[...] = layer.bias.view_as(self_layer.bias)[...]
+
+    def forward(self, x):
+        """ Pytorch forward
+
+        Args:
+            x: input image (224x224)
+
+        Returns: class logits
+
+        """
+        x = F.relu(self.conv_1_1(x))
+        x = F.relu(self.conv_1_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_2_1(x))
+        x = F.relu(self.conv_2_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_3_1(x))
+        x = F.relu(self.conv_3_2(x))
+        x = F.relu(self.conv_3_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_4_1(x))
+        x = F.relu(self.conv_4_2(x))
+        x = F.relu(self.conv_4_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_5_1(x))
+        x = F.relu(self.conv_5_2(x))
+        x = F.relu(self.conv_5_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc6(x))
+        x = F.dropout(x, 0.5)
+        x = F.relu(self.fc7(x))
+        x = F.dropout(x, 0.5)
+        return self.fc8(x)
+
+
+if __name__ == "__main__":
+    model = VGG_16()
+    model.load_weights()
+    im = cv2.imread("images/ak.png")
+    im = torch.Tensor(im).permute(2, 0, 1).view(1, 3, 224, 224)
+    import numpy as np
+    im -= torch.Tensor(np.array([129.1863,104.7624,93.5940])).view(1, 3, 1, 1)
+    preds = F.softmax(model(im), -1)
+    values, indices = preds.max(-1)
+    print(indices)
+
+
+
---
+++ pretrained/vgg_face.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget http://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_torch.tar.gz
+tar -xvf vgg_face_torch.tar.gz
