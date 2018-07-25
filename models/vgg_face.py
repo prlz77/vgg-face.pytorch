4c264060fe9a2d30c60bae993248477029cc5136
prlz77 <pau.rodri1@gmail.com>
2018-07-25 12:36:33 +0200

    add metadata

--- models/vgg_face.py
+++ models/vgg_face.py
@@ -1,20 +1,26 @@
-from torch.utils.serialization import load_lua
+# -*- coding: utf-8 -*-
+
+__author__ = "Pau Rodríguez López, ISELAB, CVC-UAB"
+__email__ = "pau.rodri1@gmail.com"
+
+import cv2
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import cv2
+from torch.utils.serialization import load_lua
 
 
 class VGG_16(nn.Module):
     """
     Main Class
     """
+
     def __init__(self):
         """
         Constructor
         """
         super().__init__()
-        self.block_size = [2,2,3,3,3]
+        self.block_size = [2, 2, 3, 3, 3]
         self.conv_1_1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
         self.conv_1_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
         self.conv_2_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
@@ -28,7 +34,7 @@ class VGG_16(nn.Module):
         self.conv_5_1 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
         self.conv_5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
         self.conv_5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
-        self.fc6 = nn.Linear(512*7*7, 4096)
+        self.fc6 = nn.Linear(512 * 7 * 7, 4096)
         self.fc7 = nn.Linear(4096, 4096)
         self.fc8 = nn.Linear(4096, 2622)
 
@@ -44,7 +50,7 @@ class VGG_16(nn.Module):
         for i, layer in enumerate(model.modules):
             if hasattr(layer, "weight"):
                 if block <= 5:
-                    self_layer = getattr(self, "conv_%d_%d" %(block, counter))
+                    self_layer = getattr(self, "conv_%d_%d" % (block, counter))
                     counter += 1
                     if counter > self.block_size[block - 1]:
                         counter = 1
@@ -52,7 +58,7 @@ class VGG_16(nn.Module):
                     self_layer.weight.data[...] = layer.weight.view_as(self_layer.weight)[...]
                     self_layer.bias.data[...] = layer.bias.view_as(self_layer.bias)[...]
                 else:
-                    self_layer = getattr(self, "fc%d" %(block))
+                    self_layer = getattr(self, "fc%d" % (block))
                     block += 1
                     self_layer.weight.data[...] = layer.weight.view_as(self_layer.weight)[...]
                     self_layer.bias.data[...] = layer.bias.view_as(self_layer.bias)[...]
@@ -98,10 +104,8 @@ if __name__ == "__main__":
     im = cv2.imread("images/ak.png")
     im = torch.Tensor(im).permute(2, 0, 1).view(1, 3, 224, 224)
     import numpy as np
-    im -= torch.Tensor(np.array([129.1863,104.7624,93.5940])).view(1, 3, 1, 1)
+
+    im -= torch.Tensor(np.array([129.1863, 104.7624, 93.5940])).view(1, 3, 1, 1)
     preds = F.softmax(model(im), -1)
     values, indices = preds.max(-1)
     print(indices)
-
-
-
